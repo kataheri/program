8bfd14b5394ffba07236acd0771964488e11f313
herisetyoaji <63384511+herisetyoaji@users.noreply.github.com>
2020-10-17 07:37:27 +0700

    Add files via upload

---
+++ app/src/main/java/id/web/koding/jadwalsholat/Kota.kt
@@ -0,0 +1,10 @@
+package id.web.koding.jadwalsholat
+
+class Kota {
+    var id: Int? = null
+    var nama: String? = null
+
+    override fun toString(): String {
+        return nama.toString()
+    }
+}
